98b970f2e49b9acf77206b147fb7543a7a4a2ad4
ksoczowka <ksoczowka.ks@gmail.com>
2023-01-27 03:00:42 +0100

    added some manual testing for api

--- app/client/app.cpp
+++ app/client/app.cpp
@@ -8,8 +8,14 @@ int main() {
     std::shared_ptr<CookiesBaker> baker = std::shared_ptr<CookiesBaker>(new CookiesBaker());
 
     baker->addChocolate();
+    baker->addCarmel();
     baker->setQuantity(4);
-    auto cookies = baker->getCookies();
+    auto cookies = baker->getCookies().value();
+
+    std::cout << "Cookies: " << cookies->quantity_ << ":\n";
+    for(const auto& ing : cookies->ingredients_) {
+        std::cout << "- " << ing << '\n';
+    }
 
     return 0;
 }
