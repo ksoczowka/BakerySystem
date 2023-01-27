b542c7ad68d30916f90e272e0c759b45b0320af2
ksoczowka <ksoczowka.ks@gmail.com>
2023-01-27 03:00:10 +0100

    refactoring to fixture (now it works :/)

--- app/api/shop/products/tests/products_tests.cpp
+++ app/api/shop/products/tests/products_tests.cpp
@@ -2,14 +2,29 @@
 
 #include "../bakers.hpp"
 
-TEST(CookiesBakerTest, setQuantityThrowsExeceptionWhenZero) {
+struct CookiesBakerTest : public testing::Test {
     CookiesBaker baker;
+};
+struct CakeBakerTest : public testing::Test {
+    CakeBaker baker;
+};
+
+// Cookie
+TEST_F(CookiesBakerTest, setQuantityThrowsExeceptionWhenZero) {
     
     ASSERT_ANY_THROW(baker.setQuantity(0));
 }
-TEST(CookiesBakerTest, setQuantityNotThrowsExceptionWhenOneOrBigger) {
-    CookiesBaker baker;
+TEST_F(CookiesBakerTest, setQuantityNotThrowsExceptionWhenOneOrBigger) {
     
     ASSERT_NO_THROW(baker.setQuantity(1));
     ASSERT_NO_THROW(baker.setQuantity(12));
+}
+
+// Cake
+TEST_F(CakeBakerTest, setFloorsThrowsExeceptionWhenZero) {
+    ASSERT_ANY_THROW(baker.setFloors(0));
+}
+TEST_F(CakeBakerTest, setFloorsNotThrowsExceptionWhenOneOrBigger) {
+    ASSERT_NO_THROW(baker.setFloors(1));
+    ASSERT_NO_THROW(baker.setFloors(12));
 }
