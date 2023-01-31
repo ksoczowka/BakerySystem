5b6638e5f898f4d2f892057cb352ff114d196c18
ksoczowka <ksoczowka.ks@gmail.com>
2023-01-31 19:20:33 +0100

    removed baker manual test

--- app/client/app.cpp
+++ app/client/app.cpp
@@ -5,19 +5,11 @@ int main() {
     std::cout << "I like trains\n";
     auto session = Session::getInstance();
 
-    std::shared_ptr<CookiesBaker> baker = std::shared_ptr<CookiesBaker>(new CookiesBaker());
+    User usr = User("db.txt");
 
-    baker->addChocolate();
-    baker->addCarmel();
-    baker->setQuantity(4);
-    auto cookies = baker->getCookies().value();
-
-    std::cout << "Cookies: " << cookies->quantity_ << ":\n";
-    for(const auto& ing : cookies->ingredients_) {
-        std::cout << "- " << ing << '\n';
+    if(usr.getUserFromDatabase("user", "12345")) {
+        
     }
-    Password pswd;
-    std::cout << pswd.decryptPassword("49-50-51-52-53/").value_or("error") << '\n';
 
     return 0;
 }
