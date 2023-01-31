0091a1050c76d09b016187da311061fafefb26dc
ksoczowka <ksoczowka.ks@gmail.com>
2023-01-31 17:38:49 +0100

    moved session to main header file

--- app/BakerySystem/BakerySystem.hpp
+++ app/BakerySystem/BakerySystem.hpp
@@ -1,7 +1,28 @@
 #ifndef BAKERY_SYSTEM_HPP_
 #define BAKERY_SYSTEM_HPP_
 
-#include "shop/shop.hpp"
-#include "login/login.hpp"
+#include "./shop/shop.hpp"
+#include "./login/user.hpp"
+
+class Session {
+public:
+    static std::shared_ptr<Session> getInstance();
+
+    // Disables copy constructor and assign operator to prevent cloning of this object
+    Session(Session& other) = delete;
+    void operator=(const Session& other) = delete;
+private:
+    Session(){}
+    static std::shared_ptr<Session> instance_;
+};
+
+std::shared_ptr<Session> Session::instance_ = nullptr;
+
+std::shared_ptr<Session> Session::getInstance() {
+    if(instance_== nullptr) {
+        instance_ = std::shared_ptr<Session>(new Session());
+    }
+    return instance_;
+}
 
 #endif // BAKERY_SYSTEM_HPP_
