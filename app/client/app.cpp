12599c2b03f10cc55d592d27ef6040381bb26e93
ksoczowka <ksoczowka.ks@gmail.com>
2023-01-31 23:28:00 +0100

    added build script

--- app/client/app.cpp
+++ app/client/app.cpp
@@ -5,11 +5,10 @@ int main() {
     std::cout << "I like trains\n";
 
     auto session = Session::getInstance();
+    session->openDatabase("./db.txt");
+    session->login("user", "12345");
 
-    session->openDatabase("db.txt");
-    if(session->login("user", "12345")) {
-        std::cout << "Logged in...\n";
-    }
+    
 
     return 0;
 }
