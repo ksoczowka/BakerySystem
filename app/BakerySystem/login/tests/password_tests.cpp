ade1ecebf4dbd834845ce35d937db027e1cccf4d
ksoczowka <ksoczowka.ks@gmail.com>
2023-01-31 17:42:39 +0100

    moved password test to dedicated file

--- app/BakerySystem/login/tests/password_tests.cpp
+++ app/BakerySystem/login/tests/password_tests.cpp
@@ -1,14 +1,13 @@
 #include <gtest/gtest.h>
 
-#include "../login.hpp"
+#include "../password.hpp"
 
-#include <memory>
 
 struct PasswordTest : public ::testing::Test {
     std::shared_ptr<Password> password = std::shared_ptr<Password>(new Password());
 };
 
-// isValid()
+// isValidPassword()
 TEST_F(PasswordTest, IsValidPasswordShouldReturnTrue) {
     EXPECT_TRUE(password->isValidPassword("12345"));
     EXPECT_TRUE(password->isValidPassword("qwert"));
