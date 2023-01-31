2ecb33d8c215490d6a4b648fa5c76d0cb2249eb8
ksoczowka <ksoczowka.ks@gmail.com>
2023-01-31 19:22:08 +0100

    removed all manual tests

--- app/client/app.cpp
+++ app/client/app.cpp
@@ -3,13 +3,6 @@
 
 int main() {
     std::cout << "I like trains\n";
-    auto session = Session::getInstance();
-
-    User usr = User("db.txt");
-
-    if(usr.getUserFromDatabase("user", "12345")) {
-        
-    }
 
     return 0;
 }
